1c9f08e8c5c1b384e1f269c2672f09529f16760a
kokoyoana <koko-yoana@hotmail.es>
2020-01-20 16:34:12 +0100

    Update prediccion.js

--- js/prediccion.js
+++ js/prediccion.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     type: "GET",
     success: function (respuesta) {
       console.log(respuesta);
-      tempInC = (respuesta.main.temp -273).toFixed(0);
+      tempInC = (respuesta.main.temp - 273).toFixed(0);
       feelsLikeInF = (respuesta.main.feels_like - 273).toFixed(0);
       $(".locName").html(respuesta.name);
       $(".weatherCondition").html(respuesta.weather[0].main);
@@ -13,7 +13,7 @@ $(document).ready(function () {
       $(".feelsLike").html(feelsLikeInF + " °C");
       $(".windSpeed").html((respuesta.wind.Speed));
       $(".todaySummary").html(respuesta.weather[0].description);
-
+      $(".weekDayTempMin1").html(respuesta.);
       /* $.each(respuesta, function (indice, valor) {
         
         $(".currentTemp").html(valor.temp);
@@ -80,21 +80,21 @@ $(document).ready(function () {
     success: function (respuesta) {
       console.log(respuesta);
       $.each(respuesta.list, function (indice, valor) {
-    
+
         $(".weekDayTempMax1").html(Math.round(Math.max(respuesta.list[0].main.temp_max, respuesta.list[1].main.temp_max, respuesta.list[2].main.temp_max, respuesta.list[3].main.temp_max,
-          respuesta.list[4].main.temp_max, respuesta.list[5].main.temp_max, respuesta.list[6].main.temp_max, respuesta.list[7].main.temp_max)-273).toFixed(0));
+          respuesta.list[4].main.temp_max, respuesta.list[5].main.temp_max, respuesta.list[6].main.temp_max, respuesta.list[7].main.temp_max) - 273).toFixed(0));
 
         $(".weekDayTempMax2").html(Math.round(Math.max(respuesta.list[8].main.temp_max, respuesta.list[9].main.temp_max, respuesta.list[10].main.temp_max, respuesta.list[11].main.temp_max,
-          respuesta.list[12].main.temp_max, respuesta.list[13].main.temp_max, respuesta.list[14].main.temp_max, respuesta.list[15].main.temp_max)-273).toFixed(0));
+          respuesta.list[12].main.temp_max, respuesta.list[13].main.temp_max, respuesta.list[14].main.temp_max, respuesta.list[15].main.temp_max) - 273).toFixed(0));
 
         $(".weekDayTempMax3").html(Math.round(Math.max(respuesta.list[16].main.temp_max, respuesta.list[17].main.temp_max, respuesta.list[18].main.temp_max, respuesta.list[19].main.temp_max,
-          respuesta.list[20].main.temp_max, respuesta.list[21].main.temp_max, respuesta.list[22].main.temp_max, respuesta.list[23].main.temp_max)-273).toFixed(0));
+          respuesta.list[20].main.temp_max, respuesta.list[21].main.temp_max, respuesta.list[22].main.temp_max, respuesta.list[23].main.temp_max) - 273).toFixed(0));
 
         $(".weekDayTempMax4").html(Math.round(Math.max(respuesta.list[24].main.temp_max, respuesta.list[25].main.temp_max, respuesta.list[26].main.temp_max, respuesta.list[27].main.temp_max,
-          respuesta.list[28].main.temp_max, respuesta.list[29].main.temp_max, respuesta.list[30].main.temp_max, respuesta.list[31].main.temp_max)-273).toFixed(0));
+          respuesta.list[28].main.temp_max, respuesta.list[29].main.temp_max, respuesta.list[30].main.temp_max, respuesta.list[31].main.temp_max) - 273).toFixed(0));
 
         $(".weekDayTempMax5").html(Math.round(Math.max(respuesta.list[32].main.temp_max, respuesta.list[33].main.temp_max, respuesta.list[34].main.temp_max, respuesta.list[35].main.temp_max,
-          respuesta.list[36].main.temp_max, respuesta.list[37].main.temp_max, respuesta.list[38].main.temp_max, respuesta.list[39].main.temp_max)-273).toFixed(0));
+          respuesta.list[36].main.temp_max, respuesta.list[37].main.temp_max, respuesta.list[38].main.temp_max, respuesta.list[39].main.temp_max) - 273).toFixed(0));
 
         //$("#dia3").html("Temperatura Actual: " + respuesta.list[0].main.temp + "<br>" + "Temperatura Mínima: " + valor.main.temp_min + "<br>" + "Temperatura Maxima: " + valor.main.temp_max);
 
@@ -118,29 +118,29 @@ $(document).ready(function () {
       console.log(respuesta);
       $.each(respuesta.list, function (indice, valor) {
 
+
         $(".weekDayTempMin1").html(Math.round(Math.min(respuesta.list[0].main.temp_min, respuesta.list[1].main.temp_min, respuesta.list[2].main.temp_min, respuesta.list[3].main.temp_min,
-          respuesta.list[4].main.temp_min, respuesta.list[5].main.temp_min, respuesta.list[6].main.temp_min, respuesta.list[7].main.temp_min)-273).toFixed(0));
+          respuesta.list[4].main.temp_min, respuesta.list[5].main.temp_min, respuesta.list[6].main.temp_min, respuesta.list[7].main.temp_min) - 273).toFixed(0));
 
         $(".weekDayTempMin2").html(Math.round(Math.min(respuesta.list[8].main.temp_min, respuesta.list[9].main.temp_min, respuesta.list[10].main.temp_min, respuesta.list[11].main.temp_min,
-          respuesta.list[12].main.temp_min, respuesta.list[13].main.temp_min, respuesta.list[14].main.temp_min, respuesta.list[15].main.temp_min)-273).toFixed(0));
+          respuesta.list[12].main.temp_min, respuesta.list[13].main.temp_min, respuesta.list[14].main.temp_min, respuesta.list[15].main.temp_min) - 273).toFixed(0));
 
         $(".weekDayTempMin3").html(Math.round(Math.min(respuesta.list[16].main.temp_min, respuesta.list[17].main.temp_min, respuesta.list[18].main.temp_min, respuesta.list[19].main.temp_min,
-          respuesta.list[20].main.temp_min, respuesta.list[21].main.temp_min, respuesta.list[22].main.temp_min, respuesta.list[23].main.temp_min)-273).toFixed(0));
+          respuesta.list[20].main.temp_min, respuesta.list[21].main.temp_min, respuesta.list[22].main.temp_min, respuesta.list[23].main.temp_min) - 273).toFixed(0));
 
         $(".weekDayTempMin4").html(Math.round(Math.min(respuesta.list[24].main.temp_min, respuesta.list[25].main.temp_min, respuesta.list[26].main.temp_min, respuesta.list[27].main.temp_min,
-          respuesta.list[28].main.temp_min, respuesta.list[29].main.temp_min, respuesta.list[30].main.temp_min, respuesta.list[31].main.temp_min)-273).toFixed(0));
+          respuesta.list[28].main.temp_min, respuesta.list[29].main.temp_min, respuesta.list[30].main.temp_min, respuesta.list[31].main.temp_min) - 273).toFixed(0));
 
         $(".weekDayTempMin5").html(Math.round(Math.min(respuesta.list[32].main.temp_min, respuesta.list[33].main.temp_min, respuesta.list[34].main.temp_min, respuesta.list[35].main.temp_min,
-          respuesta.list[36].main.temp_min, respuesta.list[37].main.temp_min, respuesta.list[38].main.temp_min, respuesta.list[39].main.temp_min)-273).toFixed(0));
-            //$("#dia3").html("Temperatura Actual: " + respuesta.list[0].main.temp + "<br>" + "Temperatura Mínima: " + valor.main.temp_min + "<br>" + "Temperatura Maxima: " + valor.main.temp_max);
+          respuesta.list[36].main.temp_min, respuesta.list[37].main.temp_min, respuesta.list[38].main.temp_min, respuesta.list[39].main.temp_min) - 273).toFixed(0));
+        //$("#dia3").html("Temperatura Actual: " + respuesta.list[0].main.temp + "<br>" + "Temperatura Mínima: " + valor.main.temp_min + "<br>" + "Temperatura Maxima: " + valor.main.temp_max);
 
-          });
-        }
-        /* error: function(jqXHR, textStatus, errorThrown){
-           console.log("ERROR MIO:  " + errorThrown);
-         }*/
-    
       });
-    
-    
-    });
+    }
+    /* error: function(jqXHR, textStatus, errorThrown){
+       console.log("ERROR MIO:  " + errorThrown);
+     }*/
+
+  });
+
+});
